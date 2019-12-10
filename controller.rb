b40bf4efc29957f90e8f3d5f4dcaaef33238d170
Luca Sanz Charreun <lucasanz2471@gmail.com>
2019-12-10 17:21:20 +0000

    MVP done, I'm really proud of myself :)

--- controller.rb
+++ controller.rb
@@ -3,8 +3,13 @@ require 'sinatra/contrib/all' if development?
 require 'pry'
 
 require_relative('./models/film')
+also_reload('./models/*')
 
-get '/index' do
+get '/' do
+  "Hello World"
+end
+
+get '/films' do
   @films = Film.all()
-  erb(:all)
+  erb(:films)
 end
